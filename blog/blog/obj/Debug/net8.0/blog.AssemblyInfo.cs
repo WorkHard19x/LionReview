ca81c8e8c27a5f455fd94f2420c4cd32f4cb0227
leo doan <workhard19x@gmail.com>
2024-05-17 06:03:28 -0500

    demo

--- blog/blog/obj/Debug/net8.0/blog.AssemblyInfo.cs
+++ blog/blog/obj/Debug/net8.0/blog.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("blog")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1c3ca0371cffe9b7b5f8901e0d14ccc9dea286e4")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a02e8ffa3383224e5d02a390d1d2c03b412fee02")]
 [assembly: System.Reflection.AssemblyProductAttribute("blog")]
 [assembly: System.Reflection.AssemblyTitleAttribute("blog")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
